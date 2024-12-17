fcb138dec68fe8d78f48461bc0a042e275c85488
EnriqueOrtiz2020630629 <lortizc1901@alumno.ipn.mx>
2024-12-17 13:08:30 -0600

    agregar parametro num_epochs en train_single_model

--- train_single_model.py
+++ train_single_model.py
@@ -8,13 +8,14 @@ if __name__ == "__main__":
     parser.add_argument('--s', type=int, default=12)
     parser.add_argument('--m', type=int, default=4)
     parser.add_argument('--num_workers', type=int, default=8)
+    parser.add_argument('--num_epochs', type=int, default=5)
     parser.add_argument('--train_file', type=str, default="T91atacado.h5")
     parser.add_argument('--eval_file', type=str, default="Set5_limpio.h5")
     parser.add_argument('--outputs_dir', type=str, default="./models")
     args = parser.parse_args()
     
 
-    model = train_model(d=56,s=12,m=4, num_workers=8, train_file="T91atacado.h5", eval_file="Set5_limpio.h5", outputs_dir="./models")
+    model = train_model(d=56,s=12,m=4, num_workers=8, num_epochs=args.num_epochs, train_file="T91atacado.h5", eval_file="Set5_limpio.h5", outputs_dir="./models")
 
     psnr_limpio = evaluate_model(model, "Set5_limpio.h5")
     psnr_atacado = evaluate_model(model, "Set5_atacado.h5")
